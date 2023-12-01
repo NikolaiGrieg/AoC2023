f21079f52320cdd4270265bfdfe8cefb2fe13edd
NikolaiGrieg <Nikolaigrieg@gmail.com>
2023-12-01 16:33:35 +0100

    small works

--- 1/Main.hs
+++ 1/Main.hs
@@ -1,16 +1,31 @@
 import Data.Char
-
+import Data.List
 
 main = do  
     contents <- readFile "small.txt"
     let l = lines  contents
-    let nums = map parseFirstNum l
+    let nums =  map  parseAllNums l
+    let ans = map stringConcatNum $ map firstAndLast nums
     print nums
+    print ans
+
+firstAndLast :: [a] -> [a]
+firstAndLast xs = head xs:[(last xs)]
+
+stringConcatNum :: [Int] -> Int
+stringConcatNum xs = read $ strConcat (map show xs)
+
+strConcat :: [String] -> String
+strConcat xs = foldr (\a b -> a ++ b) "" xs
 
 parseFirstNum :: String -> Int
 parseFirstNum (x:xs) 
     | isDigit x = digitToInt x
     | otherwise = parseFirstNum xs
+
+
+parseAllNums :: String -> [Int]
+parseAllNums s = [digitToInt x | x <- s, isDigit x]
 
 --parseNums :: String -> Int
 --parseNums s = read s
--- 1/run.sh
+++
@@ -1,1 +0,0 @@
-ghc -o main main.hs && ./main
