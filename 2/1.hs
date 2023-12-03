320a8ea4a4a0e88e9a7988a256a6c80262b32caa
NikolaiGrieg <Nikolaigrieg@gmail.com>
2023-12-03 11:07:56 +0100

    2.1 works

--- 2/1.hs
+++ 2/1.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.List.Split
+import Data.List (nub)
+
 
 data Bag = Bag{
     _id :: Int,
@@ -9,26 +11,22 @@ data Bag = Bag{
     green :: Int
 } deriving Show
 
---parse = (entryToBag . readEntry)
---splitAndParse = (\x ->  map parse $ splitOn ";" x)
---parseFull = (\x -> splitAndParse $ head $ tail $ splitOn ":" x) -- remove game id prefix
-processLine = mergeBags . parseFullWithId
-
 limit = Bag 0 14 12 13
 
 main = do  
-    contents <- readFile "small.txt"
+    contents <- readFile "large.txt"
     let l = lines  contents
-    print $ toBag 321 ["3","blue"]
-    print "--------------"
-    print $ splitAndParseWithId $ (123, [["3 blue","4 red"], ["1 red","2 green","6 blue"], ["2 green"]])
-    --print "----------------"
-    --print $ mergeBags $ concat $ parseFull "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-    --mapM_ print $  map parseFullWithId l
-    print "-------------"
-    print $ filter isPossible $ map processLine l
+    --print $ toBag 321 ["3","blue"]
+    --print "--------------"
+    --print $ splitAndParseWithId $ (123, [["3 blue","4 red"], ["1 red","2 green","6 blue"], ["2 green"]])
+    --print "-------------"
+    let bags = concat $ map parseFullWithId l
+    let invalidIds = map _id  $ filter (not . isPossible) bags
+    --print invalidIds
+    let validIds = filter (not . ( `elem` invalidIds)) $ map _id  bags 
+    --print $ validIds
     print "--------ANS----------"
-    print $ mergeBags $ filter isPossible $ map processLine l
+    print $ sum $ nub validIds
     --print "-------------"
     --print l
 
@@ -61,34 +59,22 @@ toBag id s
 isPossible :: Bag -> Bool
 isPossible b = blue b <= blue limit && red b <= red limit && green b <= green limit
 
-mergeBags :: [Bag] -> Bag
-mergeBags bs = foldr (\a b -> Bag (mergeId (_id a) (_id b)) (blue a + blue b) (red a + red b) (green a + green b) ) (Bag 0 0 0 0) bs
+--mergeBags :: [Bag] -> Bag
+--mergeBags bs = foldr (\a b -> Bag (mergeId (_id a) (_id b)) (blue a + blue b) (red a + red b) (green a + green b) ) (Bag 0 0 0 0) bs
+--
+--mergeId :: Int -> Int -> Int
+--mergeId a b
+--          | a == b = a
+--          | a == 0 = b
+--          | b == 0 = a
+--          | otherwise = a+b --todo is is a good idea?
 
-mergeId :: Int -> Int -> Int
-mergeId a b
-          | a == b = a
-          | a == 0 = b
-          | b == 0 = a
-          | otherwise = a+b --todo is is a good idea?
-
-entryToBag :: [[String]] -> [Bag]
-entryToBag ss = map (bag . toTuple) ss
 
 toTuple :: [String] -> (String, String)
 toTuple ss = (head ss, head $ tail ss) 
 
 
-bag :: (String, String) -> Bag
-bag (numStr, color)  
-                | color == "blue" = Bag 0 num 0 0
-                | color == "red" = Bag 0 0 num 0
-                | color == "green" = Bag 0 0 0 num
-                | otherwise = Bag 0 0 0 0
-                  where num = read numStr
-
 -- 3 blue, 4 red; -> [["3","blue"],["4","red"]]
 readEntry :: String -> [[String]] 
 readEntry s = map words (splitOn "," s)
 
-readId :: String -> String
-readId s = head (splitOn ":" s)
